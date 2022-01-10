00cc261ca4d880f1b02cca206e6db961c954e81c
aaditya1803 <aaditya.r2000@gmail.com>
2022-01-10 15:30:45 +0530

    added fb config

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDrUbblixmPSNrNpYZfs7a-17wSP1LBCN4",
+    authDomain: "learning-machine-alpha.firebaseapp.com",
+    projectId: "learning-machine-alpha",
+    storageBucket: "learning-machine-alpha.appspot.com",
+    messagingSenderId: "792896806273",
+    appId: "1:792896806273:web:70c36660c7355da2389c80",
+    measurementId: "G-X0FRE6RM0L"
+  };
